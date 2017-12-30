ba86e9b52927f72d082e5718c0261cd2a9d5e2ad
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2017-12-31 01:38:58 +0800

    :clock130: Initialize Solution

---
+++ Server/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace norsu.ass.Server
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
